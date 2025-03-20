fd1213a65afd975c7dd16759b580af6c5a4ff2e7
ec23268 <ec23268@qmul.ac.uk>
2025-03-20 09:42:21 +0000

    Added custom images for sidebar

--- src/components/LocationWeather.jsx
+++ src/components/LocationWeather.jsx
@@ -6,7 +6,7 @@ const LocationWeather = ({forecast, unit, onClick}) => {
   return (
     <button onClick={onClick} className={`location-weather`}>
       <h1>{Math.round(unitConversion.convertTemperature(forecast.main.temp, unit))+unitConversion.getUnitSymbol_Temperature(unit)}</h1>
-      <img src={`http://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png`} alt="weather icon"/>
+      <img src={require(`../assets/weather-icons/${forecast.weather[0].icon}.png`)} alt="weather icon"/>
       <p className="desc">{forecast.weather[0].description}</p>
       <p className="location">📍 {(forecast.name).charAt(0).toUpperCase() + (forecast.name).slice(1).toLowerCase()}</p>
     </button>
